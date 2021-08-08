827439fc88e311849b55ef6f5cb8722f535ab2f9
Théo Billardey <theo.billardey@gmail.com>
2021-08-08 23:47:13 +0200

    add poppins font

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import { tailwind } from '../../tailwind'
+import AppLoading from 'expo-app-loading'
+import { useFonts, Poppins_700Bold } from '@expo-google-fonts/poppins'
 
 const HomeScreen = () => {
-  return (
-    <>
-      <SafeAreaView
-        style={tailwind('bg-main-grey flex-1 flex justify-center items-center')}
-      >
-        <Text style={tailwind('text-white')}>Home</Text>
-      </SafeAreaView>
-    </>
-  )
+  let [fontsLoaded] = useFonts({
+    Poppins_700Bold,
+  })
+
+  if (fontsLoaded) {
+    return (
+      <>
+        <SafeAreaView
+          style={tailwind(
+            'bg-main-grey flex-1 flex justify-center items-center'
+          )}
+        >
+          <Text
+            style={{
+              ...tailwind('text-white text-4xl'),
+              fontFamily: 'Poppins_700Bold',
+            }}
+          >
+            Home
+          </Text>
+        </SafeAreaView>
+      </>
+    )
+  } else {
+    return <AppLoading />
+  }
 }
 
 export default HomeScreen
