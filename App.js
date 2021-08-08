48f46779004604672b7e24a016538145a386e19d
Théo Billardey <theo.billardey@gmail.com>
2021-08-08 21:05:27 +0200

    edit App

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { tailwind } from './tailwind'
 
 const App = () => {
   return (
     <>
-      <SafeAreaView style={tailwind('bg-main-grey flex-1')}>
-        <Text style={tailwind('text-white')}>Test</Text>
+      <SafeAreaView
+        style={tailwind('bg-main-grey flex-1 flex justify-center items-center')}
+      >
+        <Text style={tailwind('text-white font-poppins')}>Home</Text>
+        <TouchableOpacity
+          style={tailwind('px-4 py-2 bg-white rounded-xl mt-6')}
+        >
+          <Text style={tailwind('text-main-grey')}>Other page</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   )
