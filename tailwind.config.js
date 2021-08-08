f09bf4d0eece9f409bb4ca9deb36398396b83e22
Théo Billardey <theo.billardey@gmail.com>
2021-08-08 19:40:16 +0200

    add tailwind with custom config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'main-grey': '#151515',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
