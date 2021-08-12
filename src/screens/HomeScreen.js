a851693d9786764a6417846d33b0439e5ad37d5f
Théo Billardey <theo.billardey@gmail.com>
2021-08-13 01:37:30 +0200

    import BrowseSection

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,30 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { SafeAreaView, View } from 'react-native'
 import { tailwind } from '../../tailwind'
+import BrowseSection from '../components/BrowseSection'
 import Header from '../components/Header'
-import fetchData from '../api/rawg'
 
 const HomeScreen = () => {
-  const [gamesCount, setGamesCount] = useState(0)
-
-  useEffect(()=>{
-    getGamesCount()
-  }, [])
-
-  const getGamesCount = async () => {
-    try {
-      const gamesList = await fetchData(`games`)
-      const count = new Intl.NumberFormat('en-EN').format(gamesList.count)
-      setGamesCount(count)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
   return (
     <SafeAreaView style={tailwind('bg-main-grey flex-1')}>
       <View>
-        <Header gamesCount={gamesCount} />
+        <Header />
+        <BrowseSection />
       </View>
     </SafeAreaView>
   )
