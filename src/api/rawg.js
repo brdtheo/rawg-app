f030f4edee36fc2226cc9b2291917f50856fcba2
Théo Billardey <theo.billardey@gmail.com>
2021-08-12 19:45:45 +0200

    add axios config

---
+++ src/api/rawg.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import RAWG_KEY from '../../key'
+
+const fetchData = async (path) => {
+  if (path) {
+    try {
+      const data = await axios.get(
+        `https://api.rawg.io/api/${path}?key=${RAWG_KEY}`
+      )
+      return data.data
+    } catch (err) {
+      console.error(err)
+    }
+  } else {
+    console.error('cannot trigger API request: no path provided')
+  }
+}
+
+export default fetchData
