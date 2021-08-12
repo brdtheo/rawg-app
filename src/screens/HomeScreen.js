cbe988355da5b9142aebb07cfcc90126e8e317f0
Théo Billardey <theo.billardey@gmail.com>
2021-08-12 22:54:55 +0200

    edit HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,36 +1,14 @@
-import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { React } from 'react'
+import { SafeAreaView } from 'react-native'
 import { tailwind } from '../../tailwind'
-import AppLoading from 'expo-app-loading'
-import { useFonts, Poppins_700Bold } from '@expo-google-fonts/poppins'
+import Header from '../components/Header'
 
 const HomeScreen = () => {
-  let [fontsLoaded] = useFonts({
-    Poppins_700Bold,
-  })
-
-  if (fontsLoaded) {
-    return (
-      <>
-        <SafeAreaView
-          style={tailwind(
-            'bg-main-grey flex-1 flex justify-center items-center'
-          )}
-        >
-          <Text
-            style={{
-              ...tailwind('text-white text-4xl'),
-              fontFamily: 'Poppins_700Bold',
-            }}
-          >
-            Home
-          </Text>
-        </SafeAreaView>
-      </>
-    )
-  } else {
-    return <AppLoading />
-  }
+  return (
+    <SafeAreaView style={tailwind('bg-main-grey flex-1')}>
+      <Header />
+    </SafeAreaView>
+  )
 }
 
 export default HomeScreen
