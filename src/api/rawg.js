dbddfbbe27dc337c1341da16feb96fd469b302f5
Théo Billardey <theo.billardey@gmail.com>
2021-08-13 01:52:04 +0200

    edit api config

--- src/api/rawg.js
+++ src/api/rawg.js
@@ -5,7 +5,7 @@ const fetchData = async (path) => {
   if (path) {
     try {
       const data = await axios.get(
-        `https://api.rawg.io/api/${path}?key=${RAWG_KEY}`
+        `https://api.rawg.io/api/${path}&key=${RAWG_KEY}`
       )
       return data.data
     } catch (err) {
