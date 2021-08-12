26f8f353c11c99c5b97a820147e630bf5fdd84be
Théo Billardey <theo.billardey@gmail.com>
2021-08-12 22:55:24 +0200

    import SearchProvider

--- App.js
+++ App.js
@@ -3,20 +3,23 @@ import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './src/screens/HomeScreen'
+import { SearchProvider } from './src/context/SearchContext'
 
 const Stack = createStackNavigator()
 
 const AppStack = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SearchProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SearchProvider>
   )
 }
 
