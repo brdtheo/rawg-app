129245b4aee6caae228d8e9608b6baf3d91774dd
Théo Billardey <theo.billardey@gmail.com>
2021-08-14 01:46:53 +0200

    add data prop

--- src/components/GamesCarousel.js
+++ src/components/GamesCarousel.js
@@ -1,17 +1,13 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { FlatList, View, Text } from 'react-native'
 import { tailwind } from '../../tailwind'
 import { useFonts, Poppins_700Bold } from '@expo-google-fonts/poppins'
-import { SearchContext } from '../context/SearchContext'
 import GameCard from './GameCard'
 
-const GamesCarousel = ({ title }) => {
+const GamesCarousel = ({ title, data }) => {
   const [fontsLoaded] = useFonts({
     Poppins_700Bold,
   })
-
-  const { result } = useContext(SearchContext)
-  const [searchResult] = result
 
   const Item = ({ item }) => (
     <View style={tailwind('w-80 mr-6')}>
@@ -41,8 +37,8 @@ const GamesCarousel = ({ title }) => {
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={tailwind('pl-4')}
             horizontal={true}
-            data={searchResult.results}
-            keyExtractor={(item) => item.name}
+            data={data}
+            keyExtractor={(item, index) => index.toString()}
             renderItem={Item}
           />
         </View>
