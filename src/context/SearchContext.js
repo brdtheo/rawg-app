9ce2d7d07d0ff8b7aef71e97901acbb2fd4fd254
Théo Billardey <theo.billardey@gmail.com>
2021-08-13 22:49:46 +0200

    add search text to context

--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -4,9 +4,15 @@ export const SearchContext = React.createContext([])
 
 export const SearchProvider = ({ children }) => {
   const [searchResult, setSearchResult] = useState([])
+  const [searchText, setSearchText] = useState('')
 
   return (
-    <SearchContext.Provider value={{ searchResult, setSearchResult }}>
+    <SearchContext.Provider
+      value={{
+        result: [searchResult, setSearchResult],
+        text: [searchText, setSearchText],
+      }}
+    >
       {children}
     </SearchContext.Provider>
   )
