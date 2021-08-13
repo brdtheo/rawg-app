79f502fbb6fb43b065d7b3731b512c76633e1996
Théo Billardey <theo.billardey@gmail.com>
2021-08-14 00:07:11 +0200

    import GamesCarousel in HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import { SafeAreaView, View } from 'react-native'
+import React, { useContext } from 'react'
+import { SafeAreaView, ScrollView, View } from 'react-native'
 import { tailwind } from '../../tailwind'
 import BrowseSection from '../components/BrowseSection'
+import GamesCarousel from '../components/GamesCarousel'
 import Header from '../components/Header'
+import { CarouselProvider } from '../context/CarouselContext'
+import { CarouselContext } from '../context/CarouselContext'
 
 const HomeScreen = ({ navigation }) => {
+  const { newReleases, bestScore } = useContext(CarouselContext)
+  
+
   return (
-    <SafeAreaView style={tailwind('bg-main-grey flex-1')}>
-      <View>
-        <Header navigation={navigation} />
-        <BrowseSection />
-      </View>
-    </SafeAreaView>
+    <CarouselProvider>
+      <SafeAreaView style={tailwind('bg-main-grey flex-1')}>
+        <ScrollView>
+          <Header navigation={navigation} />
+          <BrowseSection />
+          <GamesCarousel title="New releases" />
+          <GamesCarousel title="Best score" />
+        </ScrollView>
+      </SafeAreaView>
+    </CarouselProvider>
   )
 }
 
