e0825503fce9ccacae8d4286782fe44f1e45ff62
Théo Billardey <theo.billardey@gmail.com>
2021-08-16 01:29:11 +0200

    add header state

--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -5,12 +5,14 @@ export const SearchContext = React.createContext([])
 export const SearchProvider = ({ children }) => {
   const [searchResult, setSearchResult] = useState([])
   const [searchText, setSearchText] = useState('')
+  const [expandHeader, setExpandHeader] = useState(true)
 
   return (
     <SearchContext.Provider
       value={{
         result: [searchResult, setSearchResult],
         text: [searchText, setSearchText],
+        header: [expandHeader, setExpandHeader],
       }}
     >
       {children}
