b7fc29ea8b97398feba9ec8d0736e042d0f37a72
Théo Billardey <theo.billardey@gmail.com>
2021-08-16 01:30:09 +0200

    add expand header button

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 import { SearchContext } from '../context/SearchContext'
 import { tailwind } from '../../tailwind'
 import {
@@ -12,11 +12,13 @@ import CategoryCard from './CategoryCard'
 import AppLoading from 'expo-app-loading'
 import { useRoute } from '@react-navigation/native'
 import { formatNumber } from '../utilities/Utils'
+import { Ionicons } from '@expo/vector-icons'
 
 const SearchResults = () => {
-  const { text, result } = useContext(SearchContext)
+  const { text, result, header } = useContext(SearchContext)
   const [searchText] = text
   const [searchResult] = result
+  const [expandHeader, setExpandHeader] = header
 
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -50,27 +52,70 @@ const SearchResults = () => {
   return (
     <>
       {fontsLoaded ? (
-        <View style={tailwind('px-4 mt-6 flex-1')}>
-          <Text
-            style={{
-              ...tailwind('text-white text-2xl'),
-              fontFamily: 'Poppins_700Bold',
-            }}
-          >
-            {formatNumber(searchResult.count)} items found
-          </Text>
+        <View
+          style={{
+            ...tailwind('px-4 flex-1'),
+            marginTop: expandHeader ? 0 : 12,
+          }}
+        >
+          <View style={tailwind('flex-row justify-between items-center')}>
+            <Text
+              style={{
+                ...tailwind('text-white text-2xl'),
+                fontFamily: 'Poppins_700Bold',
+              }}
+              numberOfLines={1}
+            >
+              {searchResult && searchResult.count
+                ? `${formatNumber(searchResult.count)} items found`
+                : 'loading data...'}
+            </Text>
 
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            data={searchResult.results}
-            keyExtractor={(item) => item.name}
-            renderItem={
-              route.params && route.params.card === 'game'
-                ? GameCardItem
-                : CategoryCardItem
-            }
-            style={tailwind('mt-4')}
-          />
+            <TouchableOpacity
+              style={tailwind(
+                'rounded-lg bg-card-grey h-8 w-8 items-center justify-center'
+              )}
+              onPress={() => setExpandHeader(!expandHeader)}
+            >
+              {expandHeader ? (
+                <Ionicons name="chevron-up" size={14} color="white" />
+              ) : (
+                <Ionicons name="chevron-down" size={14} color="white" />
+              )}
+            </TouchableOpacity>
+          </View>
+
+          {searchResult && searchResult.results ? (
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={searchResult.results}
+              keyExtractor={(item) => item.name}
+              renderItem={
+                route.params && route.params.card === 'game'
+                  ? GameCardItem
+                  : CategoryCardItem
+              }
+              style={tailwind('mt-4')}
+            />
+          ) : (
+            <View style={tailwind('w-full mt-4')}>
+              <View style={tailwind('mb-6')}>
+                <GameCard placeholder />
+              </View>
+              <View style={tailwind('mb-6')}>
+                <GameCard placeholder />
+              </View>
+              <View style={tailwind('mb-6')}>
+                <GameCard placeholder />
+              </View>
+              <View style={tailwind('mb-6')}>
+                <GameCard placeholder />
+              </View>
+              <View style={tailwind('mb-6')}>
+                <GameCard placeholder />
+              </View>
+            </View>
+          )}
         </View>
       ) : (
         <AppLoading />
