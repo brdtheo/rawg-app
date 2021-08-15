08e863f44a42a99530f9baa9200bb50312cb7636
Théo Billardey <theo.billardey@gmail.com>
2021-08-16 01:28:42 +0200

    add placeholder prop for GameCard

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -17,7 +17,7 @@ import SegaIcon from './icons/SegaIcon'
 import CommodoreIcon from './icons/CommodoreIcon'
 import ThreeDOIcon from './icons/ThreeDOIcon'
 
-const GameCard = ({ name, image, platforms, score, truncate }) => {
+const GameCard = ({ name, image, platforms, score, truncate, placeholder }) => {
   const [fontsLoaded] = useFonts({
     Poppins_700Bold,
   })
@@ -85,55 +85,76 @@ const GameCard = ({ name, image, platforms, score, truncate }) => {
             },
             shadowRadius: 4,
             elevation: 2,
+            overflow: 'hidden',
           }}
         >
           {image ? (
             <Image
-              style={tailwind('w-full h-36 rounded-t-2xl')}
+              style={tailwind('w-full h-36')}
               source={{
                 uri: image,
               }}
             />
           ) : null}
+          {placeholder ? (
+            <View style={tailwind('w-full h-36 bg-gray-200')} />
+          ) : null}
           <View style={tailwind('p-4 w-full')}>
-            <View
-              style={tailwind(
-                'w-full flex flex-row justify-between items-center mb-2'
-              )}
-            >
-              {platforms ? (
-                <View style={tailwind('flex flex-row')}>
-                  {platforms.map((platform) => getIcon(platform.platform.name))}
-                </View>
-              ) : null}
-              {score ? (
-                <View
-                  style={{
-                    ...tailwind('flex px-1 border rounded'),
-                    borderColor: getScoreColor(score),
-                  }}
-                >
-                  <Text
+            {placeholder ? (
+              <View
+                style={tailwind(
+                  'w-full flex flex-row justify-between items-center mb-2'
+                )}
+              >
+                <View style={tailwind('rounded bg-gray-200 w-24 h-6')} />
+                <View style={tailwind('rounded bg-gray-200 w-12 h-6')} />
+              </View>
+            ) : (
+              <View
+                style={tailwind(
+                  'w-full flex flex-row justify-between items-center mb-2'
+                )}
+              >
+                {platforms ? (
+                  <View style={tailwind('flex flex-row')}>
+                    {platforms.map((platform) =>
+                      getIcon(platform.platform.name)
+                    )}
+                  </View>
+                ) : null}
+                {score ? (
+                  <View
                     style={{
-                      color: getScoreColor(score),
-                      fontFamily: 'Poppins_700Bold',
+                      ...tailwind('flex px-1 border rounded'),
+                      borderColor: getScoreColor(score),
                     }}
                   >
-                    {score}
-                  </Text>
-                </View>
-              ) : null}
-            </View>
+                    <Text
+                      style={{
+                        color: getScoreColor(score),
+                        fontFamily: 'Poppins_700Bold',
+                      }}
+                    >
+                      {score}
+                    </Text>
+                  </View>
+                ) : null}
+              </View>
+            )}
             <View style={tailwind('w-full')}>
-              <Text
-                numberOfLines={truncate ? 1 : null}
-                style={{
-                  ...tailwind('text-xl text-white'),
-                  fontFamily: 'Poppins_700Bold',
-                }}
-              >
-                {name}
-              </Text>
+              {placeholder ? (
+                <View style={tailwind('rounded w-full h-6 bg-gray-200')} />
+              ) : (
+                <Text
+                  numberOfLines={truncate ? 1 : null}
+                  style={{
+                    ...tailwind('text-xl text-white'),
+                    fontFamily: 'Poppins_700Bold',
+                  }}
+                >
+                  {name}
+                </Text>
+              )}
             </View>
           </View>
         </TouchableOpacity>
