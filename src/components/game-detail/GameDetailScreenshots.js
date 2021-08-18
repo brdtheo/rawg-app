315b35a1899a6b04831845ac77bf41fcdb8ef112
Théo Billardey <theo.billardey@gmail.com>
2021-08-19 00:13:14 +0200

    create GameDetailScreenshots

---
+++ src/components/game-detail/GameDetailScreenshots.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { View, FlatList, TouchableOpacity, Image } from 'react-native'
+import { tailwind } from '../../../tailwind'
+
+const GameDetailScreenshots = ({ data }) => {
+  const ScreenshotItem = ({ item }) => (
+    <TouchableOpacity style={tailwind('mr-6 w-60 h-full')}>
+      <Image
+        source={{ uri: item.image }}
+        style={tailwind('w-full h-full rounded-lg')}
+      />
+    </TouchableOpacity>
+  )
+
+  return (
+    <View style={tailwind('w-full mb-6')}>
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={tailwind('pl-4 h-36')}
+        horizontal={true}
+        data={data}
+        keyExtractor={(item) => item.image}
+        renderItem={ScreenshotItem}
+      />
+    </View>
+  )
+}
+
+export default GameDetailScreenshots
