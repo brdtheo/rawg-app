df3dc28856462ef638d1a711ebd7b0843389d470
Théo Billardey <theo.billardey@gmail.com>
2021-08-19 01:23:59 +0200

    create InfoGridItem lcoal component

--- src/components/detail-screen/GameDetailInfoGrid.js
+++ src/components/detail-screen/GameDetailInfoGrid.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Linking } from 'react-native'
 import {
   useFonts,
   Poppins_400Regular,
@@ -26,197 +26,119 @@ const GameDetailInfoGrid = ({
     Poppins_700Bold,
   })
 
+  const InfoGridItem = ({ title, children }) => (
+    <View style={{ ...tailwind('mb-6'), width: '46%' }}>
+      <Text
+        style={{
+          ...tailwind('text-white text-opacity-40'),
+          fontFamily: 'Poppins_500Medium',
+        }}
+      >
+        {title}
+      </Text>
+      <View style={tailwind('mt-2 flex-row flex-wrap')}>{children}</View>
+    </View>
+  )
+
   return (
     <>
       {fontsLoaded ? (
         <View style={tailwind('mx-4 mb-6 flex-row justify-between flex-wrap')}>
-          <View style={{ ...tailwind('mb-6'), width: '46%' }}>
-            <Text
-              style={{
-                ...tailwind('text-white text-opacity-40'),
-                fontFamily: 'Poppins_500Medium',
-              }}
-            >
-              Platforms
-            </Text>
-            <View style={tailwind('mt-2 flex-row flex-wrap')}>
-              {platforms.map((p, index) => (
-                <Text
-                  style={{
-                    ...tailwind('text-white underline'),
-                    marginRight: index === platforms.length - 1 ? 0 : 5,
-                    marginBottom: index === platforms.length - 1 ? 0 : 5,
-                  }}
-                >
-                  {p.platform.name}
-                  {index === platforms.length - 1 ? '' : ','}
-                </Text>
-              ))}
-            </View>
-          </View>
-
-          {metacritic ? (
-            <View style={{ ...tailwind('mb-6'), width: '46%' }}>
+          <InfoGridItem title="Platforms">
+            {platforms.map((p, index) => (
               <Text
                 style={{
-                  ...tailwind('text-white text-opacity-40'),
-                  fontFamily: 'Poppins_500Medium',
+                  ...tailwind('text-white underline'),
+                  marginRight: index === platforms.length - 1 ? 0 : 5,
+                  marginBottom: index === platforms.length - 1 ? 0 : 5,
                 }}
               >
-                Metascore
+                {p.platform.name}
+                {index === platforms.length - 1 ? '' : ','}
               </Text>
-              <View style={tailwind('mt-2 flex-row flex-wrap')}>
-                <View
+            ))}
+          </InfoGridItem>
+
+          {metacritic ? (
+            <InfoGridItem title="Metascore">
+              <View
+                style={{
+                  ...tailwind('flex px-1 border rounded'),
+                  borderColor: getScoreColor(metacritic),
+                }}
+              >
+                <Text
                   style={{
-                    ...tailwind('flex px-1 border rounded'),
-                    borderColor: getScoreColor(metacritic),
+                    color: getScoreColor(metacritic),
+                    fontFamily: 'Poppins_700Bold',
                   }}
                 >
-                  <Text
-                    style={{
-                      color: getScoreColor(metacritic),
-                      fontFamily: 'Poppins_700Bold',
-                    }}
-                  >
-                    {metacritic}
-                  </Text>
-                </View>
+                  {metacritic}
+                </Text>
               </View>
-            </View>
+            </InfoGridItem>
           ) : null}
 
           {genres ? (
-            <View style={{ ...tailwind('mb-6'), width: '46%' }}>
-              <Text
-                style={{
-                  ...tailwind('text-white text-opacity-40'),
-                  fontFamily: 'Poppins_500Medium',
-                }}
-              >
-                Genres
-              </Text>
-              <View style={tailwind('mt-2 flex-row flex-wrap')}>
-                {genres.map((g, index) => (
-                  <Text
-                    style={{
-                      ...tailwind('text-white underline'),
-                      marginRight: index === genres.length - 1 ? 0 : 5,
-                      marginBottom: index === genres.length - 1 ? 0 : 5,
-                    }}
-                  >
-                    {g.name}
-                    {index === genres.length - 1 ? '' : ','}
-                  </Text>
-                ))}
-              </View>
-            </View>
+            <InfoGridItem title="Genres">
+              {genres.map((g, index) => (
+                <Text
+                  style={{
+                    ...tailwind('text-white underline'),
+                    marginRight: index === genres.length - 1 ? 0 : 5,
+                    marginBottom: index === genres.length - 1 ? 0 : 5,
+                  }}
+                >
+                  {g.name}
+                  {index === genres.length - 1 ? '' : ','}
+                </Text>
+              ))}
+            </InfoGridItem>
           ) : null}
 
           {releaseDate ? (
-            <View style={{ ...tailwind('mb-6'), width: '46%' }}>
-              <Text
-                style={{
-                  ...tailwind('text-white text-opacity-40'),
-                  fontFamily: 'Poppins_500Medium',
-                }}
-              >
-                Release date
+            <InfoGridItem title="Release date">
+              <Text style={tailwind('text-white')}>
+                {formatDate(releaseDate)}
               </Text>
-              <View style={tailwind('mt-2 flex-row flex-wrap')}>
-                <Text style={tailwind('text-white')}>
-                  {formatDate(releaseDate)}
-                </Text>
-              </View>
-            </View>
+            </InfoGridItem>
           ) : null}
 
           {updateDate ? (
-            <View style={{ ...tailwind('mb-6'), width: '46%' }}>
-              <Text
-                style={{
-                  ...tailwind('text-white text-opacity-40'),
-                  fontFamily: 'Poppins_500Medium',
-                }}
-              >
-                Updated
+            <InfoGridItem title="Updated">
+              <Text style={tailwind('text-white')}>
+                {formatDate(updateDate)}
               </Text>
-              <View style={tailwind('mt-2 flex-row flex-wrap')}>
-                <Text style={tailwind('text-white')}>
-                  {formatDate(updateDate)}
-                </Text>
-              </View>
-            </View>
+            </InfoGridItem>
           ) : null}
 
           {playtime ? (
-            <View style={{ ...tailwind('mb-6'), width: '46%' }}>
-              <Text
-                style={{
-                  ...tailwind('text-white text-opacity-40'),
-                  fontFamily: 'Poppins_500Medium',
-                }}
-              >
-                Average playtime
+            <InfoGridItem title="Average playtime">
+              <Text style={tailwind('text-white')}>
+                {playtime} hour
+                {playtime > 1 ? 's' : null}
               </Text>
-              <View style={tailwind('mt-2 flex-row flex-wrap')}>
-                <Text style={tailwind('text-white')}>
-                  {playtime} hour
-                  {playtime > 1 ? 's' : null}
-                </Text>
-              </View>
-            </View>
+            </InfoGridItem>
           ) : null}
 
           {website ? (
-            <View style={{ ...tailwind('mb-6'), width: '46%' }}>
-              <Text
-                style={{
-                  ...tailwind('text-white text-opacity-40'),
-                  fontFamily: 'Poppins_500Medium',
-                }}
-              >
-                Website
-              </Text>
-              <View style={tailwind('mt-2 flex-row flex-wrap')}>
-                <TouchableOpacity onPress={() => Linking.openURL(website)}>
-                  <Text style={tailwind('text-white underline')}>
-                    {website}
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+            <InfoGridItem title="Website">
+              <TouchableOpacity onPress={() => Linking.openURL(website)}>
+                <Text style={tailwind('text-white underline')}>{website}</Text>
+              </TouchableOpacity>
+            </InfoGridItem>
           ) : null}
 
           {achievementsCount ? (
-            <View style={{ ...tailwind('mb-6'), width: '46%' }}>
-              <Text
-                style={{
-                  ...tailwind('text-white text-opacity-40'),
-                  fontFamily: 'Poppins_500Medium',
-                }}
-              >
-                Achievements
-              </Text>
-              <View style={tailwind('mt-2 flex-row flex-wrap')}>
-                <Text style={tailwind('text-white')}>{achievementsCount}</Text>
-              </View>
-            </View>
+            <InfoGridItem title="Achievements">
+              <Text style={tailwind('text-white')}>{achievementsCount}</Text>
+            </InfoGridItem>
           ) : null}
 
           {esrb ? (
-            <View style={{ ...tailwind('mb-6'), width: '46%' }}>
-              <Text
-                style={{
-                  ...tailwind('text-white text-opacity-40'),
-                  fontFamily: 'Poppins_500Medium',
-                }}
-              >
-                ESRB Rating
-              </Text>
-              <View style={tailwind('mt-2 flex-row flex-wrap')}>
-                <Text style={tailwind('text-white')}>{esrb.name}</Text>
-              </View>
-            </View>
+            <InfoGridItem title="ESRB Rating">
+              <Text style={tailwind('text-white')}>{esrb.name}</Text>
+            </InfoGridItem>
           ) : null}
         </View>
       ) : null}
