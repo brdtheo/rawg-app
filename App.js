52a380fab0db28b709901a60bd228dc6a4f1d012
Théo Billardey <theo.billardey@gmail.com>
2021-08-18 22:52:19 +0200

    import HomeProvider

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './src/screens/HomeScreen'
 import { SearchProvider } from './src/context/SearchContext'
+import { HomeProvider } from './src/context/HomeContext'
 import SearchScreen from './src/screens/SearchScreen'
 import DetailScreen from './src/screens/DetailScreen'
 
@@ -11,19 +12,21 @@ const Stack = createStackNavigator()
 
 const AppStack = () => {
   return (
-    <SearchProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Search" component={SearchScreen} />
-          <Stack.Screen name="Detail" component={DetailScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SearchProvider>
+    <HomeProvider>
+      <SearchProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Search" component={SearchScreen} />
+            <Stack.Screen name="Detail" component={DetailScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SearchProvider>
+    </HomeProvider>
   )
 }
 
