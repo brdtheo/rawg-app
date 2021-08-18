66844665f8c37bfee636d0a5739ad4a5d0c0546f
Théo Billardey <theo.billardey@gmail.com>
2021-08-19 00:08:26 +0200

    create GameDetailHeader

---
+++ src/components/game-detail/GameDetailHeader.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import moment from 'moment'
+import { getIcon } from '../../utilities/Utils'
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins'
+import { tailwind } from '../../../tailwind'
+
+const GameDetailHeader = ({ name, releaseDate, platforms }) => {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_700Bold,
+  })
+
+  return (
+    <>
+      {fontsLoaded ? (
+        <View>
+          <View style={tailwind('mx-4 flex-row w-full items-center')}>
+            {releaseDate ? (
+              <View style={tailwind('bg-white rounded px-2 py-1 mr-2')}>
+                <Text
+                  style={{
+                    ...tailwind('text-main-grey tracking-widest text-xs'),
+                    fontFamily: 'Poppins_400Regular',
+                  }}
+                >
+                  {moment(releaseDate).format('MMM D, YYYY')}
+                </Text>
+              </View>
+            ) : null}
+            <View style={tailwind('flex-row')}>
+              {platforms.map((platform) => getIcon(platform.platform.name))}
+            </View>
+          </View>
+
+          <Text
+            style={{
+              ...tailwind('mx-4 text-white text-3xl mb-2 w-full pt-3 pb-4'),
+              fontFamily: 'Poppins_700Bold',
+            }}
+          >
+            {name}
+          </Text>
+        </View>
+      ) : null}
+    </>
+  )
+}
+
+export default GameDetailHeader
