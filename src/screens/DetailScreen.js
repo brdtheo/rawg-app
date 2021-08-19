84357761da9a16d390f21ae5f0c182651a1c7180
Théo Billardey <theo.billardey@gmail.com>
2021-08-19 19:13:10 +0200

    import GameDetailReviews

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,37 +1,29 @@
 import React, { useState, useEffect, useContext } from 'react'
-import {
-  View,
-  Text,
-  SafeAreaView,
-  ActivityIndicator,
-  ScrollView,
-  TouchableOpacity,
-} from 'react-native'
+import { View, SafeAreaView, ActivityIndicator, ScrollView } from 'react-native'
 import { tailwind } from '../../tailwind'
 import fetchData from '../api/rawg'
 import Header from '../components/Header'
 import { SearchContext } from '../context/SearchContext'
 import { useRoute } from '@react-navigation/native'
-import { getScoreColor } from '../utilities/Utils'
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins'
-import moment from 'moment'
 import axios from 'axios'
-import { Linking } from 'react-native'
 import GameDetailHeader from '../components/detail-screen/GameDetailHeader'
 import GameDetailScreenshots from '../components/detail-screen/GameDetailScreenshots'
 import GameDetailStores from '../components/detail-screen/GameDetailStores'
 import GameDetailAbout from '../components/detail-screen/GameDetailAbout'
 import GameDetailInfoGrid from '../components/detail-screen/GameDetailInfoGrid'
+import GameDetailReviews from '../components/detail-screen/GameDetailReviews'
 
 const GameDetail = () => {
   const [gameData, setGameData] = useState(null)
   const [gameScreenshots, setGameScreenshots] = useState(null)
   const [gameStores, setGameStores] = useState(null)
+  const [gameReviews, setGameReviews] = useState(null)
   const { header } = useContext(SearchContext)
   const [expandHeader, setExpandHeader] = header
   const gameSlug = useRoute().params.slug
@@ -44,6 +36,7 @@ const GameDetail = () => {
       setGameData(null)
       setGameScreenshots(null)
       setGameStores(null)
+      setGameReviews(null)
     }
   }, [])
 
@@ -53,11 +46,13 @@ const GameDetail = () => {
         fetchData(`games/${gameSlug}`, '?'),
         fetchData(`games/${gameSlug}/screenshots`, '?'),
         fetchData(`games/${gameSlug}/stores`, '?'),
+        fetchData(`games/${gameSlug}/reviews`, '?'),
       ])
 
       setGameData(gameData[0])
       setGameScreenshots(gameData[1])
       setGameStores(gameData[2])
+      setGameReviews(gameData[3])
     } catch (err) {
       console.error(err)
     }
@@ -73,7 +68,11 @@ const GameDetail = () => {
     <SafeAreaView style={tailwind('bg-main-grey flex-1')}>
       <View style={tailwind('flex-1')}>
         <Header arrow />
-        {gameData && gameScreenshots && gameStores && fontsLoaded ? (
+        {gameData &&
+        gameScreenshots &&
+        gameStores &&
+        gameReviews &&
+        fontsLoaded ? (
           <ScrollView style={tailwind('mt-4 flex-1')}>
             <GameDetailHeader
               releaseDate={gameData.released}
@@ -102,6 +101,10 @@ const GameDetail = () => {
               achievementsCount={gameData.achievements_count}
               esrb={gameData.esrb_rating}
             />
+
+            {gameReviews.results.length ? (
+              <GameDetailReviews reviews={gameReviews.results} />
+            ) : null}
           </ScrollView>
         ) : (
           <View style={tailwind('justify-center items-center flex-1')}>
