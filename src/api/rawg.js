fdb4b9ea0c711fb52e1f64dd3926dd43b1f8eff6
Théo Billardey <theo.billardey@gmail.com>
2021-08-21 02:18:32 +0200

    edit api config

--- src/api/rawg.js
+++ src/api/rawg.js
@@ -1,22 +1,23 @@
 import axios from 'axios'
-const RAWG_KEY = process.env.RAWG_KEY
+import apiKeys from '../../key'
+const API_KEY = process.env.RAWG_KEY ?? apiKeys.RAWG_KEY
 
 const fetchData = async (path, keyChar) => {
   if (path && keyChar) {
     try {
       const data = await axios.get(
-        `https://api.rawg.io/api/${path}${keyChar}key=${RAWG_KEY}`
+        `https://api.rawg.io/api/${path}${keyChar}key=${API_KEY}`
       )
       return data.data
     } catch (err) {
-      console.error(err)
+      console.error(err, API_KEY)
     }
   } else if (path && !keyChar) {
     try {
       const data = await axios.get(path)
       return data.data
     } catch (err) {
-      console.error(err)
+      console.error(err, API_KEY)
     }
   } else {
     console.error('cannot trigger API request: no path or keyChar provided')
